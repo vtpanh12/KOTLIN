f57f660ee3c22fc15f7b006fe64fb5e95b77c4d6
vtpa <phuonganh.ct2002@gmail.com>
2024-03-05 23:35:11 +0700

    display

---
+++ app/src/main/java/com/example/database_basic/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.database_basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.database_basic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
